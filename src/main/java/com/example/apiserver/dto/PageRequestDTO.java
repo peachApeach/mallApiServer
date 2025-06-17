b55389db8cefc8832ce35feb96b7f04d877eb70d
peachApeach <yh_01@daum.net>
2025-06-17 20:52:30 +0900

    ADD: dto for paging

--- src/main/java/com/example/apiserver/dto/PageRequestDTO.java
+++ src/main/java/com/example/apiserver/dto/PageRequestDTO.java
@@ -1,9 +1,15 @@
 package com.example.apiserver.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageRequestDTO {
 
     @Builder.Default
--- src/main/java/com/example/apiserver/dto/PageResponseDTO.java
+++ src/main/java/com/example/apiserver/dto/PageResponseDTO.java
@@ -1,5 +1,48 @@
 package com.example.apiserver.dto;
 
-public class PageResponseDTO {
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Data
+// 페이징 결과물 (list dto)
+public class PageResponseDTO<E> {
+    private List<E> dtoList;
+
+    private List<Integer> pageNumList;
+
+    private PageRequestDTO pageRequestDTO;
+
+    // 이전, 다음 버튼
+    private boolean prev, next;
+    
+    private int totalCount, prevPage, nextPage, totalPage, current;
+
+    public PageResponseDTO(List<E> dtoList, PageRequestDTO pageRequestDTO, long total){
+        this.dtoList = dtoList;
+        this.pageRequestDTO = pageRequestDTO;
+        this.totalCount = (int)total;
+
+        // end 페이지 계산
+        int end = (int)(Math.ceil(pageRequestDTO.getPage() / 10.0) * 10);
+
+        int start = end - 9;
+
+        //total end 페이지 계산
+        int last = (int)(Math.ceil(totalCount/(double)pageRequestDTO.getSize()));
+
+        end = end > last ? last : end;
+
+        this.prev = start>1;
+        this.next = totalCount > end * pageRequestDTO.getSize();
+
+        this.pageNumList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+
+        this.prevPage = prev ? start -1 : 0;
+
+        this.nextPage = next ? end + 1 : 0;
+    }
 }
